abd870e7bdf3ca511bea08cdeec2bb068c990cbe
Evy38 <cecilemorel0907@gmail.com>
2025-06-26 17:46:59 +0200

    feat(auth): ajout des champs de confirmation email et mot de passe avec validation dans le formulaire d'inscription

--- src/app/homepage/components/signup-modal/signup-modal.component.ts
+++ src/app/homepage/components/signup-modal/signup-modal.component.ts
@@ -16,12 +16,15 @@ export class SignupModalComponent {
   submitted = false;
 
   constructor(private readonly fb: FormBuilder) {
-    this.signupForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required]
-    });
+this.signupForm = this.fb.group({
+  email: ['', [Validators.required, Validators.email]],
+  emailConfirm: ['', [Validators.required, Validators.email]],
+  password: ['', [Validators.required, Validators.minLength(6)]],
+  passwordConfirm: ['', [Validators.required]],
+  firstName: ['', Validators.required],
+  lastName: ['', Validators.required]
+}, { validators: [this.matchEmails, this.matchPasswords] });
+
   }
 
   onSubmit() {
@@ -37,4 +40,16 @@ export class SignupModalComponent {
   onClose() {
     this.close.emit();
   }
+  matchEmails(group: FormGroup) {
+  const email = group.get('email')?.value;
+  const emailConfirm = group.get('emailConfirm')?.value;
+  return email === emailConfirm ? null : { emailMismatch: true };
+}
+
+matchPasswords(group: FormGroup) {
+  const pass = group.get('password')?.value;
+  const passConfirm = group.get('passwordConfirm')?.value;
+  return pass === passConfirm ? null : { passwordMismatch: true };
+}
+
 }
