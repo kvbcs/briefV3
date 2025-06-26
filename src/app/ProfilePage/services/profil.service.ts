5888f1d57dda1dda1ad9030031c0adc8b3a8e510
Imane <barbecheimane@gmail.com>
2025-06-26 14:56:36 +0200

    feat: added user model and profile service to mock a user

--- src/app/ProfilePage/services/profil.service.ts
+++ src/app/ProfilePage/services/profil.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../models/user.model';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfilService {
+  private userMock: User = {
+    lastname: 'Dupont',
+    firstname: 'Marie',
+    email: 'marie.dupont@example.com',
+    createdAt: new Date('2024-01-01'),
+    cguAcceptedAt: new Date('2024-01-01'),
+  };
 
-  constructor() { }
+  getUser(): Observable<User> {
+    return of(this.userMock);
+  }
 }
