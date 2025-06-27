8d946500879e7b0d0a337ddf3ac0d1eb2e4a528a
KUISSI <gkuissi@gmail.com>
2025-06-27 16:24:02 +0200

    list&preview&detail

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { AuthService } from './gk/services/auth.service';
+import { ListService } from './gk/services/list.service';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { AUTH_SERVICE } from './gk/tokens';
+
+providers: [
+  { provide: AUTH_SERVICE, useClass: AuthService }
+]
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
+ListService,{ provide: AUTH_SERVICE, useClass: AuthService }]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ListsComponent } from './lists/lists.component';
+import { ListDetailComponent } from './list-detail/list-detail.component';
+import { ListPreviewComponent } from './list-preview/list-preview.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'lists', component: ListsComponent },
+  {
+    path: 'lists/:id',
+    component: ListDetailComponent,
+  },
+  { path: 'list-preview/:id', component: ListPreviewComponent },
+  { path: '**', redirectTo: '' },
+];
