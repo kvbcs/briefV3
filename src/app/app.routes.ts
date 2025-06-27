8d508cd50cdb858944a406a42a352e299888b1d0
Imane <barbecheimane@gmail.com>
2025-06-27 11:51:40 +0200

    feat(sidebar): create mobile-first sidebar with toggle button and conditional menu based on user role

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //autres routes ici
+  { path: 'sidebar', component: SidebarComponent },
+];
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  role = 'user'; // à remplacer plus tard par un service Auth ou un signal
+  isOpen = signal(false); // par défaut la sidebar est cachée
 
+  toggleSidebar(): void {
+    this.isOpen.update((prev) => !prev);
+  }
+
+  logout() {
+    console.log('Déconnexion');
+    // redirect ou clear storage ici plus tard
+  }
 }
