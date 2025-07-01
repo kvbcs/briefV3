b6857a0a3c428fb5e0db6ce889b45a54fe89efc7
Kyllian <kyllianvibancos2@gmail.com>
2025-07-01 09:56:22 +0200

    feature : edit site title

--- src/app/homepage/components/login-form/login-form.component.ts
+++ src/app/homepage/components/login-form/login-form.component.ts
@@ -1,18 +1,40 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, Output, EventEmitter, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { FakeAuthService } from '../../../core/services/fake-auth.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
+  editMode: boolean = false;
+  title = signal<string>(localStorage.getItem('siteTitle') || 'ShuffleMyTeam');
+  tempTitle = signal<string>(this.title());
 
+  editTitle() {
+    this.tempTitle.set(this.title());
+    this.editMode = true;
+  }
+
+  saveTitle() {
+    const newTitle = this.tempTitle().trim();
+    if (newTitle.length > 0) {
+      this.title.set(newTitle);
+      localStorage.setItem('siteTitle', newTitle);
+    } else {
+      alert("Titre invalide")
+    }
+    this.editMode = false;
+  }
   // Formulaire réactif avec 2 champs
   loginForm: FormGroup;
 
@@ -20,10 +42,14 @@ export class LoginFormComponent {
   @Output() openSignup = new EventEmitter<void>();
   errorMessage: string = '';
 
-  constructor(private readonly fb: FormBuilder, private readonly auth: FakeAuthService, private readonly router: Router) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly auth: FakeAuthService,
+    private readonly router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required]],
     });
   }
 
@@ -33,21 +59,18 @@ export class LoginFormComponent {
     console.log('Tentative de connexion avec :', email, password); // 👈 TEST 1
 
     this.auth.login(email, password).subscribe({
-      next: user => {
+      next: (user) => {
         console.log('Connexion réussie :', user); // 👈 TEST 2
         this.errorMessage = '';
       },
-      error: err => {
+      error: (err) => {
         console.error('Erreur de connexion :', err); // 👈 TEST 3
         this.errorMessage = 'Email ou mot de passe incorrect';
-      }
+      },
     });
   }
-
 
   onSignupClick() {
     this.openSignup.emit(); // ✅ dit au parent "ouvre la modale"
   }
-
-
 }
